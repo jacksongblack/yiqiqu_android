8489d796951395f6d33c06ecdb1dd229a8182c17
Jack <songyuchaoemail@163.com>
2015-12-24 23:09:14 +0800

    第一初始化项目

---
+++ app/src/main/java/com/example/jack/yiqiqu/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.jack.yiqiqu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import android.util.Log;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class MainActivity extends AppCompatActivity {
+    private InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("没有连接成功");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if(response == HttpURLConnection.HTTP_OK){
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("连接正常",ex.getLocalizedMessage());
+            throw new IOException("错误连接");
+        }
+        return in;
+    }
+
+    private String WordDfinition(String word){
+        InputStream in = null;
+        String strDfinition = "";
+        try{
+            in = OpenHttpConnection("http://"+word);
+            Document doc = null;
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db;
+            try{
+                db = dbf.newDocumentBuilder();
+                doc = db.parse(in);
+            }catch(ParserConfigurationException e){
+                e.printStackTrace();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            doc.getDocumentElement().normalize();
+            NodeList definitionElements = doc.getElementsByTagName("Definition");
+            for (int i = 0;i<definitionElements.getLength();i++){
+                Node itemsNode = definitionElements.item(i);
+                if (itemsNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element definitionElement = (Element) itemsNode;
+                    NodeList wordDefinitionElements = (definitionElements).getElementsByTagName("WordDefinition");
+                    strDfinition = "";
+                    for(int j = 0; j<wor)
+                }
+            }
+      }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
