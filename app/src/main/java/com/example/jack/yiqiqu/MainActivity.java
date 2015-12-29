fbf3ebb388b16f796b8dcd188074960f6edf6b8a
Jack <songyuchaoemail@163.com>
2015-12-30 05:35:11 +0800

    修改安卓

--- app/src/main/java/com/example/jack/yiqiqu/MainActivity.java
+++ app/src/main/java/com/example/jack/yiqiqu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jack.yiqiqu;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+
 import android.util.Log;
+import android.widget.Toast;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -31,61 +36,77 @@ public class MainActivity extends AppCompatActivity {
         URLConnection conn = url.openConnection();
         if (!(conn instanceof HttpURLConnection))
             throw new IOException("没有连接成功");
-        try{
+        try {
             HttpURLConnection httpConn = (HttpURLConnection) conn;
             httpConn.setAllowUserInteraction(false);
             httpConn.setInstanceFollowRedirects(true);
             httpConn.setRequestMethod("GET");
             httpConn.connect();
             response = httpConn.getResponseCode();
-            if(response == HttpURLConnection.HTTP_OK){
+            if (response == HttpURLConnection.HTTP_OK) {
                 in = httpConn.getInputStream();
             }
-        }
-        catch (Exception ex)
-        {
-            Log.d("连接正常",ex.getLocalizedMessage());
+        } catch (Exception ex) {
+            Log.d("连接正常", ex.getLocalizedMessage());
             throw new IOException("错误连接");
         }
         return in;
     }
 
-    private String WordDfinition(String word){
+    private String WordDfinition(String word) {
         InputStream in = null;
         String strDfinition = "";
-        try{
-            in = OpenHttpConnection("http://"+word);
+        try {
+            in = OpenHttpConnection("http://" + word);
             Document doc = null;
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db;
-            try{
+            try {
                 db = dbf.newDocumentBuilder();
                 doc = db.parse(in);
-            }catch(ParserConfigurationException e){
+            } catch (ParserConfigurationException e) {
                 e.printStackTrace();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             doc.getDocumentElement().normalize();
             NodeList definitionElements = doc.getElementsByTagName("Definition");
-            for (int i = 0;i<definitionElements.getLength();i++){
+            for (int i = 0; i < definitionElements.getLength(); i++) {
                 Node itemsNode = definitionElements.item(i);
-                if (itemsNode.getNodeType() == Node.ELEMENT_NODE){
+                if (itemsNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element definitionElement = (Element) itemsNode;
-                    NodeList wordDefinitionElements = (definitionElements).getElementsByTagName("WordDefinition");
+                    NodeList wordDefinitionElements = (definitionElement).getElementsByTagName("WordDefinition");
                     strDfinition = "";
-                    for(int j = 0; j<wor)
+                    for (int j = 0; j < wordDefinitionElements.getLength(); j++) {
+                        Element wordDefinitionElement = (Element) wordDefinitionElements.item(j);
+                        NodeList textNodes = ((Node) wordDefinitionElement).getChildNodes();
+                        strDfinition += ((Node) textNodes.item(0)).getNodeType() + ".\n";
+                    }
                 }
             }
-      }
+        } catch (IOException e1) {
+            Log.d("NetworkingActivity", e1.getLocalizedMessage());
+        }
+        return strDfinition;
     }
+
+    private class AccessWebServiceTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return WordDfinition(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
